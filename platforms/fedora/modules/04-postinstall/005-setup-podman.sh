ea80b715a00d676a0bc74ef55dea1db2d56594e9
Emmanuel Etti <emmanueletti@gmail.com>
2026-07-31 09:00:07 -0400

    fedora: let rootless podman bind port 80

--- platforms/fedora/modules/04-postinstall/005-setup-podman.sh
+++ platforms/fedora/modules/04-postinstall/005-setup-podman.sh
@@ -8,3 +8,13 @@ if ! command -v podman >/dev/null 2>&1; then
 fi
 
 systemctl --user enable --now podman.socket
+
+# Rootless podman cannot bind below 1024 due to legacy linux reasons.
+# Tools like DDEV's router needs 80/443
+sudo tee /etc/sysctl.d/99-podman-unprivileged-ports.conf >/dev/null <<'EOF'
+net.ipv4.ip_unprivileged_port_start=80
+EOF
+
+sudo sysctl -q net.ipv4.ip_unprivileged_port_start=80
+
+echo "✅ box: podman ready"
