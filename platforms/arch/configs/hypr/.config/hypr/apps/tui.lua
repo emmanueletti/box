46980c62fe3db65cbe45f0d48ef2cc18d84d5ceb
Emmanuel Etti <emmanueletti@gmail.com>
2026-08-21 23:03:11 -0400

    arch: add TUI floating windowrule

---
+++ platforms/arch/configs/hypr/.config/hypr/apps/tui.lua
@@ -0,0 +1,11 @@
+hl.window_rule({
+  match = { tag = "floating-window" },
+  float = true,
+  center = true,
+  size = {875, 600},
+})
+
+hl.window_rule({
+  match = { class = "TUI.float" },
+  tag = "+floating-window",
+})
